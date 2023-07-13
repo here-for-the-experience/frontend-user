acaf88c707db669c20583177479056bc4e667bff
Raufun Nazin Srizon <raufun.nazin13@gmail.com>
2023-07-14 04:17:15 +0600

    styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import vaccineData from "../vaccineData";
 import { useGlobalState } from "../Context";
 import api from "../api";
 
