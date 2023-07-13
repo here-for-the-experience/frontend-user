89e41efb0fe9016af222b37346e8cce65d20809d
Raufun Nazin Srizon <raufun.nazin13@gmail.com>
2023-07-13 11:34:04 +0600

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Login from "./Login";
 
 function App() {
-
   return (
-    <div></div>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
